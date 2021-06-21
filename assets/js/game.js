d047a50395d66213eda09f1c6d61b09a08de252e
Deku <zachdsamson@gmail.com>
2021-06-20 23:33:08 -0700

    finished the fight() function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,15 @@
-window.alert("This is an alert! JavaScript is running!")
+var playerName = window.prompt("What is yourrobot's name?");
+console.log(playerName);
+
+console.log("this logs a string, good for leaving yourself a message");
+// this will do math and "log" 20
+console.log(10+10);
+// what is this? 
+console.log("our robot's name is " + playerName);
+
+// this creates a function named "fight"
+function fight() {
+    window.alert("The fight has begun!");
+};
+
+//fight();
