0034c00d58881ea83ef0813fc5b503af560db2dc
Deku <zachdsamson@gmail.com>
2021-06-27 21:24:03 -0700

    fixed null player name

--- assets/js/game.js
+++ assets/js/game.js
@@ -80,7 +80,6 @@ var randomNumber = function(min, max) {
       if (playerInfo.health > 0) {
         // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
         window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
-  
         // pick new enemy to fight based on the index of the enemyInfo array
         var pickedEnemyObj = enemyInfo[i];
   
@@ -165,13 +164,25 @@ var randomNumber = function(min, max) {
     }
   };
   
+  // funtion to set name
+  var getPlayerName = function() {
+    var name = "";
+
+    while (name === "" || name === null) {
+      name = prompt("What is your robot's name?")
+    }
+
+    console.log("Your robot's name is " + name);
+    return name;
+  };
+
   /* END GAME FUNCTIONS */
   
   /* GAME INFORMATION / VARIABLES */
   
   // player information
   var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
